0d9d2350d63a1d45fe10b1f5d472e7ff24dd7e7a
Savya Sachi V B <savyanio@gmail.com>
2021-07-01 16:26:14 +0530

    minor changes

--- insertion_sort.py
+++ insertion_sort.py
@@ -11,8 +11,7 @@ def changeRun_inserion(run):
 
 def insertion_sort(s,drawData, timeTick):
     for i in range(0,len(s)-1):
-        drawData(s, ['yellow' if x == i or x == i +
-         1 else 'red' for x in range(len(s))])
+        drawData(s, ['yellow' if x == i  else 'red' for x in range(len(s))])
         time.sleep(timeTick)
         if s[i]>s[i+1]:
             s[i],s[i+1]=s[i+1],s[i]
@@ -22,8 +21,7 @@ def insertion_sort(s,drawData, timeTick):
             for j in range(i,0,-1):
                 if s[j]<s[j-1]:
                     s[j],s[j-1]=s[j-1],s[j]
-                    drawData(s, ['green' if x == j-1
-                              else 'red' for x in range(len(s))])
+                    drawData(s, ['green' if x == j-1 else 'red' for x in range(len(s))])
                     time.sleep(timeTick)
 
 
@@ -35,7 +33,7 @@ def insertion_sort_step(s,drawData):
         print("step called")
         print(running)
         for i in range(0,len(s)-1):
-            drawData(s, ['yellow' if x == i or x == i + 1 else 'red' for x in range(len(s))])
+            drawData(s, ['yellow' if x == i else 'red' for x in range(len(s))])
             time.sleep(2)
             if(running):
                 print("step2 called")
